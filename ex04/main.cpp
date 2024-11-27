038aaf9b2a2ea64bdfd3233e770f6537de8a562d
Hicham Es-Saquy <hes-saqu@c3r8p9.1337.ma>
2024-11-27 12:56:55 +0100

    new

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,6 +3,36 @@
 #include <string>
 #include <cstdio>
 
+void replace_word(const std::string& line, const std::string& s1, const std::string& s2)
+{
+    // given a line string replaces every occurence of s1 by s2
+    // line should have at least one s1 instance
+    size_t  total_l;
+    int     numberOfs1 = 0;
+    int     s1Pos = 0;
+    int     i = 0;
+    int j = 0;
+    while (s1Pos != std::string::npos)
+    {
+        s1Pos = line.find(s1, s1Pos);
+        if (s1Pos != std::string::npos)
+        {
+            numberOfs1++;
+            s1Pos += s1.length();
+        }
+    }
+    total_l = line.length() - s1.length() * numberOfs1 + s2.length() * numberOfs1;
+    char* str = new char[total_l + 1];
+    str[total_l] = 0;
+    while (str[i] != 0)
+    {
+        while (line[j] != 0 && str[i] != 0 && line[j] != s1[0])
+            str[i++] = line[j++]; //:D . work with line.find
+        while ()
+    }
+    //std::cout << "replaced string is " << *replaced << std::endl;
+}
+
 int main(int ac, char **av) // filename s1 s2
 {
     /*It will open the file <filename> and copies its content into a new file
@@ -26,16 +56,19 @@ int main(int ac, char **av) // filename s1 s2
     while (std::getline(ifs, line))
     {
         line += '\n';
-        if (line.find(to_find)) != std::string::npos)
+        if (line.find(to_find) != std::string::npos)
         {
-            std::cout << "s1 wasnt found" << to_find << std::endl;
+            std::cout << "s1 was found" << to_find << std::endl;
+            replace_word(line, av[2], av[3]);
             ofs << line;
         }
-        buffer += line;
+        else
+            ofs << line;
+        //buffer += line;
     }
     if (buffer.length() > 0) // trim last newline
         buffer.resize(buffer.length() - 1);
-    ofs << buffer;
+    //ofs << buffer;
     ifs.close();
     ofs.close();
 }
